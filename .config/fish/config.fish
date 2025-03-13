4ed5e2297dac0edbbefd5c86b1e37fc31f6a6691
Miguel Ángel Monreal Velasco <developer@mkmonreal.com>
2025-03-13 02:17:10 +0100

    Fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,5 @@
-/home/mkmonreal/.config/fish/config.fish
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+oh-my-posh init fish --config ~/Git/dracula/oh-my-posh/dracula.omp.json | source
