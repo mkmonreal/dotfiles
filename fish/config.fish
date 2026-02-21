810d5875415b90e433abdf3e3d328fdb5ee52e91
mkmonreal <developer@mkmonreal.com>
2026-02-21 01:52:22 +0100

    Add nvm to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -9,8 +9,16 @@ end
 oh-my-posh init fish --config 'dracula' | source
 fastfetch
 
+#nvm
+function nvm
+    bass source ~/.config/nvm/nvm.sh --no-use ';' nvm $argv
+end
+
+set -x NVM_DIR ~/.nvm
+#nvm end
+
 # pnpm
-set -gx PNPM_HOME "/home/mkmonreal/.local/share/pnpm"
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
